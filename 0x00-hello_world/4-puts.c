6e25cf34664ede08225c979de56183d6efa537a0
kenne1483 <invacact@gmail.com>
2023-03-16 19:13:27 +0000

    Fixed code

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,7 @@
 
 /**
  * main - Prints "Programming is like building a multilingual
- *                puzzle, followed by a new line.
+ * puzzle, followed by a new line.
  *
  * Return: Always 0.
  */
@@ -11,4 +11,4 @@ int main(void)
     puts("\"Programming is like building a multilingual puzzle");
 
     return (0);
-}		
+}
