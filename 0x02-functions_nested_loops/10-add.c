a30d51636b3de69feee7e1fd32e0d30baaddb806
kenne1483 <invacact@gmail.com>
2023-03-22 21:47:22 +0000

    Adding 0-104 fiels

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+*add - Adds two integers and returns the result
+*
+*@num1: First integer to be added
+*@num2: Second integer to be added
+*
+*Return: result of the add
+*/
+int add(int num1, int num2)
+{
+	return (num1 + num2);
+}
+
